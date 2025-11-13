73a5cbd1d4c3022becadc21e58969ac790bebbfd
kartik08 <kartikmeena925@gmail.com>
2025-11-13 14:56:33 +0530

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,1 +1,30 @@
 
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <sqlite3.h>
+#include <fstream>
+using namespace std;
+
+class User {
+public:
+    string name;
+    string username;
+    string dob;
+    string gender;
+    
+    User() {}
+    
+    User(string n, string u, string d, string g) 
+        : name(n), username(u), dob(d), gender(g) {}
+    
+    void display() const {
+        cout << "Name: " << name << endl;
+        cout << "Username: @" << username << endl;
+        cout << "DOB: " << dob << endl;
+        cout << "Gender: " << gender << endl;
+    }
+};
